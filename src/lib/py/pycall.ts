158999c989e2877899f80e69dd681bedb94bd050
Lacy Morrow <me@lacymorrow.com>
2023-11-18 12:29:57 -0500

    tewk

--- src/lib/py/pycall.ts
+++ src/lib/py/pycall.ts
@@ -27,62 +27,62 @@ import config from '../../utils/config';
 import { retryOperation } from '../../utils/utils';
 
 declare global {
-	interface Window {
-		pywebview?: any;
-	}
+  interface Window {
+    pywebview?: any;
+  }
 }
 
 /* Python API -> Shell Connection */
 const pycall = (endpoint: string, params: any = null) => {
-	return retryOperation(
-		async () => {
-			try {
-				if (process.env.NODE_ENV === 'development') {
-					throw new Error('Not running in production, skipping pycall');
-				}
+  return retryOperation(
+    async () => {
+      try {
+        if (process.env.NODE_ENV === 'development') {
+          throw new Error('Not running in production, skipping pycall');
+        }
 
-				if (!window.pywebview?.api?.[endpoint]) {
-					throw new Error('API endpoint not found');
-				}
+        if (!window.pywebview?.api?.[endpoint]) {
+          throw new Error('API endpoint not found');
+        }
 
-				await pylog(`PyCall ${endpoint}`, params);
-				const response: string | { message: string } =
-					await window.pywebview.api[endpoint](params)
-						.then((res: any) => JSON.parse(res))
-						.then(async (res: any) => {
-							// Response contained an error
-							if (res?.error) {
-								throw res.error;
-							}
+        await pylog(`PyCall ${endpoint}`, params);
+        const response: string | { message: string } =
+          await window.pywebview.api[endpoint](params)
+            .then((res: any) => JSON.parse(res))
+            .then(async (res: any) => {
+              // Response contained an error
+              if (res?.error) {
+                throw res.error;
+              }
 
-							if (res.message === undefined) {
-								throw new Error(`Response message was empty ${res}`);
-							}
+              if (res.message === undefined) {
+                throw new Error(`Response message was empty ${res}`);
+              }
 
-							await pylog(`Pycall ${endpoint} returned: ${res.message}`, res);
-							return res.message;
-						});
-				return response;
-			} catch (error) {
-				let errorMessage = `${error}`;
-				if (error instanceof Error) {
-					errorMessage = error.message;
-				}
-				await pylog(`Pycall ${endpoint} (retrying): ${errorMessage}`);
-				throw new Error(errorMessage);
-			}
-		},
-		config.RETRY_DELAY,
-		process.env.NODE_ENV === 'development' ? 1 : config.MAX_RETRIES, // 1 retry in dev
-	).catch(async (error) => {
-		// Operation failed
-		let errorMessage = `${endpoint} - ${error}`;
-		if (error instanceof Error) {
-			errorMessage = error.message;
-		}
-		await pylog(`Pycall ${endpoint} - FATAL ERROR: ${errorMessage}`);
-		throw new Error(errorMessage);
-	});
+              await pylog(`Pycall ${endpoint} returned: ${res.message}`, res);
+              return res.message;
+            });
+        return response;
+      } catch (error) {
+        let errorMessage = `${error}`;
+        if (error instanceof Error) {
+          errorMessage = error.message;
+        }
+        await pylog(`Pycall ${endpoint} (retrying): ${errorMessage}`);
+        throw new Error(errorMessage);
+      }
+    },
+    config.RETRY_DELAY,
+    process.env.NODE_ENV === 'development' ? 1 : config.MAX_RETRIES, // 1 retry in dev
+  ).catch(async (error) => {
+    // Operation failed
+    let errorMessage = `${endpoint} - ${error}`;
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+    await pylog(`Pycall ${endpoint} - FATAL ERROR: ${errorMessage}`);
+    throw new Error(errorMessage);
+  });
 };
 
 export default pycall;
